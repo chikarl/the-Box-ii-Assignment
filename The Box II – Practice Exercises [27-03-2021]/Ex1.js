297a8ec840397fc8185f641e9c312ee6a62381d2
chi karl <kchi8573@gmail.com>
2021-06-26 06:31:24 +0100

    Delete Not needed files

--- The Box II – Practice Exercises [27-03-2021]/Ex1.js
+++
@@ -1,9 +0,0 @@
-
-
-function cm() {
-    var feet, inches, cm;
-    feet = Number(document.cal.feet.value)
-    inches = Number(document.cal.inches.value)
-    cm = (30.48*feet)+(2.54*inches)
-    document.getElementById("outputCM").innerHTML=cm+' cm';
-}
